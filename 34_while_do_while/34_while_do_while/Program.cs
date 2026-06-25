d713e46e9c9a9500c340c3ab33ffe2ad37bcb98b
gjparts <gjparts@gmail.com>
2026-06-24 19:34:52 -0600

    commit 2406202626145

---
+++ 34_while_do_while/34_while_do_while/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _34_while_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Haga un programa que capture N cantidad de edades de personas.
+            -> El programa termina cuando se digite una edad menor que cero.
+            -> Una vez termine el programa debe imprimir una tabla que resuma
+               cuantas edades se leyeron por grupo de edad de acuerdo a la escala
+               siguiente:
+
+               de 0 a menos de 18	menores de edad
+               de 18 a menos de 21  mayores de edad
+               de 21 a menos de 60  adultos
+               de 60 en adelante	tercera edad*/
+            try
+            {
+                int edad = 0; //variable bandera
+                int menores = 0, mayores = 0, adultos = 0, tercera = 0; //acumuladores
+
+                while (edad >= 0)
+                {
+                    Console.Write("Digite una edad: ");
+                    edad = int.Parse(Console.ReadLine());
+
+                    //clasificar la informacion
+                    if (edad >= 0 && edad < 18) menores++; //menores = menores + 1;
+                    if (edad >= 18 && edad < 21) mayores++;
+                    if (edad >= 21 && edad < 60) adultos++;
+                    if (edad >= 60) tercera++;
+                }
+                //sumario:
+                Console.WriteLine($"Menores de edad: {menores}");
+                Console.WriteLine($"Mayores de edad: {mayores}");
+                Console.WriteLine($"Adultos: {adultos}");
+                Console.WriteLine($"Tercera edad: {tercera}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solo se permite numeros enteros.");
+            }
+        }
+    }
+}
---
+++ 35_while_do_while/35_while_do_while/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _35_while_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Haga un menú en pantalla que muestre las opciones siguientes:
+
+            ------ MENU ------
+            1) Saludame
+            2) Dime la fecha y hora
+            3) Cuentame un chiste
+            4) Reproduce un sonido
+            5) Quiero que me ayudes a sumar dos numeros
+            6) Salir
+
+            -> Programe algo relacionado para cada acción, el programa no debe
+               terminar hasta que elija la opcion 6.
+            -> Debe limpiar la pantalla antes de mostrar el menu
+            -> Cuando se digite un numero de opcion se debera volver a mostrar el menu
+               excepto cuando se digite la opcion 6, ya que este termina con el programa.
+*/
+
+            int opcion;
+            do
+            {
+                try
+                {
+                    Console.Clear(); //limpieza de pantalla
+                    Console.WriteLine("------ MENU ------");
+                    Console.WriteLine("1) Saludame");
+                    Console.WriteLine("2) Dime la fecha y hora");
+                    Console.WriteLine("3) Cuentame un chiste");
+                    Console.WriteLine("4) Reproduce un sonido");
+                    Console.WriteLine("5) Quiero que me ayudes a sumar dos numeros");
+                    Console.WriteLine("6) Salir");
+                    Console.Write("Digite el numero de opcion deseado: ");
+                    opcion = int.Parse(Console.ReadLine());
+
+                    //las variables que use dentro de switch es recomendado
+                    //declararlas fuera de él
+                    double a, b;
+
+                    //acciones
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Hola como estas!");
+                            break;
+                        case 2:
+                            Console.WriteLine($"Hoy es: {DateTime.Now}");
+                            break;
+                        case 3:
+                            Console.WriteLine("Soy una maquina no se chistes.");
+                            break;
+                        case 4:
+                            Console.Beep(1000, 2000); //1000 Hertz durante 2 segundos
+                            Console.Beep(500, 1000); //500 Hertz durante un segundo
+                            break;
+                        case 5:
+                            Console.Write("Digite a: ");
+                            a = double.Parse(Console.ReadLine());
+                            Console.Write("Digite b: ");
+                            b = double.Parse(Console.ReadLine());
+                            Console.WriteLine($"La suma es {a+b}");
+                            break;
+                        default:
+                            Console.WriteLine("Opcion no valida");
+                            break;
+                    }
+                    Console.ReadKey(false);//hacer una pausa
+                    Console.WriteLine("Presiona cualquier tecla para volver al menu.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Numero digitado no valido.");
+                    opcion = 0; //en caso de error debe haber al menos un valor en opcion
+                }
+            } while (opcion != 6);
+        }
+    }
+}

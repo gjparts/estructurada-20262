9d2ffc4afded42f1fe6d5ada3baffa1da02e2860
gjparts <gjparts@gmail.com>
2026-06-30 10:51:26 -0600

    commit 3006202610830

---
+++ 38_control_fuljo/38_control_fuljo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _38_control_fuljo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Instruccion break: termina con la estr. de repeticion (bucle)
+             Ejemplo: Haga una estructura for que lea 10 numeros long positivos.
+            -> Si durante la digitacion se ingresa un numero negativo
+               entonces se termina la lectura de numeros.
+            -> Al finalizar el programa muestre la suma de los numeros leidos.*/
+            try
+            {
+                long numero; //variable bandera
+                long suma = 0; //variable acumulador
+                for(int i = 1; i <= 10; i++)
+                {
+                    Console.Write("Digite un entero positivo: ");
+                    numero = long.Parse(Console.ReadLine());
+
+                    if (numero < 0) break; //rompemos la estructura for
+                    suma += numero;
+                }
+                Console.WriteLine($"La suma de enteros positivos es: {suma}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Solo se acepta numeros enteros.");
+            }
+        }
+    }
+}

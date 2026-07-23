28437fc1431601a3a70051732323448d609d156d
GJParts <39961515+gjparts@users.noreply.github.com>
2026-07-23 10:49:04 -0600

    commit 2307202611668

---
+++ 50_String/50_String/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _50_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Método Substring
+            Devuelve un string que dentro de otro string.
+            -> en C# hay dos formas basicas de usarlo:
+            1) Substring(posicion_inicial)
+               Devuelve un String a partir de la posicion inicial hasta
+               llegar al final.
+            2) Substring(posicion_inicial, cantidad_caracteres)
+               Devuelve un String a partir de la posicion inicial hasta
+               llegar a la cantidad de caracteres definidos.*/
+            string frase = "El veloz murcielago hindu comia feliz cardillo y kiwi";
+            Console.WriteLine(frase);
+
+            //Substring de 9 caracteres a partir de la posicion 3
+            Console.WriteLine(frase.Substring(3,9));
+
+            //el resultado de Substring tambien puede almacenarlo
+            //en una variable, por ejemplo tomar 5 caracteres a partir
+            //de la posicion 10
+            string fragmento = frase.Substring(10, 5);
+            Console.WriteLine(fragmento);
+
+            //que pasa cuando Substring quiere tener acceso a una posicion
+            //que no existe?
+            //por ejemplo, obtener 1000 caracteres desde la posicion 1
+            try
+            {
+                Console.WriteLine(frase.Substring(1,1000));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Posicion no existe dentro de frase");
+            }
+            //si Usted sospecha que Substring puede llegar a querer acceder
+            //a una posicion inexistente, lo recomendado es colocarlo
+            //dentro de una estructura try-catch
+
+            //Substring usando unicamente la posicion inicial
+            //obtener todo el texto a partie de la posicion 12
+            Console.WriteLine(frase.Substring(12));
+            //tambien se puede guardar en una variable:
+            //tomar todo el texto desde el caracter 20 y colocarlo en una variable
+            string parte = frase.Substring(20);
+            Console.WriteLine(parte);
+        }
+    }
+}
---
+++ 51_String/51_String/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _51_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Método Replace
+            Devuelve una copia del string a la que se le ha reemplazado
+            todas las coincidencias encontradas de otro string.
+            -> Este metodo no ignora mayusc./minusc. de forma tradicional*/
+            string frase = "Verde es la Manzana, la manzana es verde. Que verde es la manzana!";
+            Console.WriteLine(frase);
+
+            //reemplazar verde por roja sin alterar el string original, imprimir el resultado
+            Console.WriteLine(frase.Replace("verde","roja"));
+            //verifiquemos si se altero el string original?
+            Console.WriteLine(frase);
+            //si quiere alterar el string original:
+            frase = frase.Replace("verde","roja");
+            //verifiquemos si se altero el string original?
+            Console.WriteLine(frase);
+
+            //Tambien es posible desencadenar varios Replace en una sola instruccion:
+            string texto = "La naranja es una buena fruta, el jugo de naranja es mejor que el de noni";
+            //reemplazar naranja por melon y reemplazar noni por aloe alterando el string original
+            texto = texto.Replace("naranja", "melon").Replace("noni","aloe");
+            Console.WriteLine(texto);
+
+            //Usar Replace pero ignorando mayusculas y minusculas
+            string colores = "Rojo ROJO ROJo RoJo AZUL AZul azul AzUL VERde VERDE verde VERdE";
+            //reemplazar azul por celeste ignorando mayusculas y minusculas alterando el string original
+            //vamos a ocupar una clase llamada Regex usada para aplicacion de expresiones regulares
+            colores = Regex.Replace(colores, "azul", "celeste", RegexOptions.IgnoreCase);
+            Console.WriteLine(colores);
+
+            /*Pregunte un string al usuario, luego reemplace en dicho string los
+             espacios en blanco por guiones bajos, la letras E por numeros 3, las
+             letras I por numeros 1.
+            -> debera ignorar mayusculas/minusculas.
+            -> imprima el string modificado*/
+            Console.Write("Digite un string: ");
+            string algo = Console.ReadLine();
+
+            //forma 1:
+            //algo = algo.Replace(" ", "_").Replace("E","3").Replace("e","3").Replace("I", "1").Replace("i", "1");
+
+            //forma 2:
+            algo = algo.Replace(" ", "_");
+            algo = Regex.Replace(algo, "E", "3", RegexOptions.IgnoreCase);
+            algo = Regex.Replace(algo, "I", "1", RegexOptions.IgnoreCase);
+            Console.WriteLine($"Resultado: {algo}");
+        }
+    }
+}
